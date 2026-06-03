4d535089d0717a2a0946eed61f14e26a2bdd5769
Licona13 <kevinliconasilva@gmail.com>
2026-06-03 15:31:57 -0600

    feature/cats: agregar endpoint get /cats

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Cat } from './entities/cat.entity';
+import { CreateCatDto } from './dtos/create-cat.dto';
+import { UpdateCatDto } from './dtos/update-cat.dto';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+
+    private cats: Cat[] = [
+        { id: 1, name: 'Boris', age: 3, weight: 4, color: 'blanco' },
+        { id: 2, name: 'Cristobal', age: 5, weight: 3.5, color: 'negro' },
+        { id: 3, name: 'Bicho', age: 1, weight: 2, color: 'naranja' },
+    ];
+
+    findAll(): Cat[] {
+        return this.cats;
+    }
+
+
+
+}
+
